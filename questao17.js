6739c27c80a10d1f6ac491a4db6485b995ac5d48
Agnny <asss6@aluno.ifal.edu.br>
2024-12-02 19:42:16 -0300

    questao17.js

---
+++ questao17.js
@@ -0,0 +1,8 @@
+let valorCompra = parseFloat(prompt('Digite o valor de compra do produto:'));
+    let valorVenda;
+    if (valorCompra < 20) {
+        valorVenda = (valorCompra * 0.45) + valorCompra; 
+    } else {
+        valorVenda = (valorCompra * 0.30) + valorCompra;
+    }
+    alert("Sua compra ficou no valor de: "+ valorVenda)
